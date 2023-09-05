33a0560b5316d4586beac903bc1e278f2f66cb84
Vyshnavi <141427880+Vyshumanchikanti@users.noreply.github.com>
2023-09-05 19:52:05 +0530

    Update FCFS.cpp

--- FCFS.cpp
+++ FCFS.cpp
@@ -1,9 +1,8 @@
 #include<stdio.h> 
-#include<conio.h> 
 main()
 {
 	int bt[20], wt[20], tat[20], i, n; 
-	float wtavg, tatavg,clrscr();
+	float wtavg, tatavg;
 	printf("\nEnter the number of processes -- "); 
 	scanf("%d", &n);
 	for(i=0;i<n;i++)
@@ -25,5 +24,4 @@ main()
 		printf("\n\t P%d \t\t %d \t\t %d \t\t %d", i, bt[i], wt[i], tat[i]); 
 		printf("\nAverage Waiting Time -- %f", wtavg/n);
 printf("\nAverage Turnaround Time -- %f", tatavg/n); 
-getch();
 }
